87943c404bdb56bc7a99341b219b425f9a0dd315
AI Pro Desk <tuyen.tuyentruongthanh@gmail.com>
2023-09-17 12:52:04 +0700

    Update technical ver2

--- app.sh
+++ app.sh
@@ -2,3 +2,11 @@ git init
 git add .
 git commit -m "Update technical ver2"
 git push
+echo "# updatest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tuyenubuntu/updatest.git
+git push -u origin main
+
